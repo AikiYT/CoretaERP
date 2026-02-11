2e902e9856276b0a159d0a333cf55d5661bcae94
Anderson-BM <andersonbatistamateo@gmail.com>
2026-02-10 23:41:04 -0400

    Continuacion de Identity

--- CoretaERP.Application/Interfaces/Services/IAccountService.cs
+++ CoretaERP.Application/Interfaces/Services/IAccountService.cs
@@ -8,11 +8,11 @@ namespace CoretaERP.Application.Interfaces.Services
 {
     public interface IAccountService
     {
-       // Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request);
+        Task<AuthenticationResponse> AuthenticateAsync(AuthenticationRequest request);
         Task<string> ConfirmAccountAsync(string userId, string token);
-    //    Task<ForgotPasswordResponse> ForgotPasswordAsync(ForgotPasswordRequest request, string origin);
-      //  Task<RegisterResponse> RegisterBasicUserAsync(RegisterRequest request, string origin);
-     //   Task<ResetPasswordResponse> ResetPasswordAsync(ResetPasswordRequest request);
+        Task<ForgotPasswordResponse> ForgotPasswordAsync(ForgotPasswordRequest request, string origin);
+        Task<RegisterResponse> RegisterBasicUserAsync(RegisterRequest request, string origin);
+        Task<ResetPasswordResponse> ResetPasswordAsync(ResetPasswordRequest request);
         Task SignOutAsync();
     }
 }
